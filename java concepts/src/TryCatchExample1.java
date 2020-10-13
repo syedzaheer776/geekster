640c8640213c76a3d28a42b05c04165aa3cd9fc9
Admin <Admin@DESKTOP-OIGOK3I>
2020-10-13 14:23:40 +0530

    third commit

--- java concepts/src/TryCatchExample1.java
+++ java concepts/src/TryCatchExample1.java
@@ -1,4 +1,19 @@
 
 public class TryCatchExample1 {
+	  public static void main(String[] args) {  
+	        try  
+	        {  
+	        int data=50/0;    
+	  
+	        }  
+	              
+	        catch(ArrayIndexOutOfBoundsException e)  
+	        {  
+	            System.out.println(e);  
+	        }  
+	        System.out.println("rest of the code");  
+	    }  
+	      
+	}  
 
-}
+
