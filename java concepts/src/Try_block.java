27f56dc07c04f20647347217e1ae3d57fa8f261f
Admin <Admin@DESKTOP-OIGOK3I>
2020-10-13 13:57:36 +0530

    first commit

---
+++ java concepts/src/Try_block.java
@@ -0,0 +1,22 @@
+
+public class Try_block {
+
+	public static void main(String[] args) {
+		
+	    try  
+        {
+	    	int data1=50/0;
+        }  
+                 
+        catch(Exception e)  
+        {  
+            System.out.println(e);  
+        }  
+        System.out.println("rest of the code");  
+    }  
+      
+  
+
+	}
+
+
