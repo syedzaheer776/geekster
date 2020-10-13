29c59c50e6025849af5d7b1e1551cd60dd5c7e7f
Admin <Admin@DESKTOP-OIGOK3I>
2020-10-13 14:19:28 +0530

    second commit

---
+++ java concepts/src/TryCatchExample.java
@@ -0,0 +1,21 @@
+
+public class TryCatchExample {
+	  public static void main(String[] args) {  
+          
+	        try  
+	        {  
+	        int data1=50/0;    
+	  
+	        }  
+	               
+	        catch(Exception e)  
+	        {  
+	              
+	        int data2=50/0;  
+	  
+	        }  
+	    System.out.println("rest of the code");  
+	    }  
+	}  
+
+
