a3b77d4dea3b8c81d4c8e81ecb1f23e91f342b1e
Admin <Admin@DESKTOP-OIGOK3I>
2020-10-21 11:08:00 +0530

    fifteenth commit

---
+++ java concepts/src/DateAndTime.java
@@ -0,0 +1,10 @@
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;  
+public class DateAndTime {
+	 public static void main(String[] args) {  
+		   DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		   LocalDateTime now = LocalDateTime.now();
+		   System.out.println(dtf.format(now));
+		  }  
+
+}
