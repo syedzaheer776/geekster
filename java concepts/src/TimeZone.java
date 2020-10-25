3f21c5e1c4c596772a588edeb226559b9f5cb217
Admin <Admin@DESKTOP-OIGOK3I>
2020-10-25 14:54:57 +0530

    twentieth commit

---
+++ java concepts/src/TimeZone.java
@@ -0,0 +1,11 @@
+import java.util.*;  
+public class TimeZone {
+	 public static void main( String args[] ){         
+		   String[] id = TimeZone.getAvailableIDs();        
+		   System.out.println("In TimeZone class available Ids are: ");  
+		   for (int i=0; i<id.length; i++){  
+		   System.out.println(id[i]);  
+		   }   
+		   }      
+
+}
