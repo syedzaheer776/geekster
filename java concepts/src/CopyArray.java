cdfd3bbc2f3f72b7a928299d8e7b45e0002c3738
Admin <Admin@DESKTOP-OIGOK3I>
2020-10-27 17:25:47 +0530

    twenty seventh commit

---
+++ java concepts/src/CopyArray.java
@@ -0,0 +1,15 @@
+
+public class CopyArray {
+	  public static void main(String[] args) {  
+	          
+	        char[] copyFrom = { 'd', 'e', 'c', 'a', 'f', 'f', 'e',  
+	                'i', 'n', 'a', 't', 'e', 'd' };  
+	          
+	        char[] copyTo = new char[7];  
+	          
+	        System.arraycopy(copyFrom, 2, copyTo, 0, 7);  
+	          
+	        System.out.println(String.valueOf(copyTo));  
+	    }  
+	}  
+
