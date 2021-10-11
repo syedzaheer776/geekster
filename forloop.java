2e126718f8cff1480f3016e603885d61a70b35f5
syed zaheer <71685879+syedzaheer776@users.noreply.github.com>
2021-10-11 22:04:47 +0530

    Create forloop.java

---
+++ forloop.java
@@ -0,0 +1,13 @@
+package Java;
+
+public class For_Loop {
+	public static void main(String args[]) {
+	
+		int Sum = 0;
+		int i;
+		for(i=0;i<5;i++) {
+			Sum++;
+			System.out.println(Sum);
+		}
+	}
+}
