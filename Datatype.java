00398c9f47584ef8aecea68b94c5301e1ab3ce43
syed zaheer <71685879+syedzaheer776@users.noreply.github.com>
2021-10-11 22:03:00 +0530

    Create Datatype.java

---
+++ Datatype.java
@@ -0,0 +1,22 @@
+package Java;
+
+public class Data_Type {
+
+	public static void main(String[] args) {
+		byte a=10;
+		short b=9;
+		int c=32000;
+		long d=543543545;
+		float e=10.989f;
+		double f=12.99878869;
+		String g="Hey! Aditya this side.";
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+	}
+
+}
